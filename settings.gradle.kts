c380036760a564f4578306e7ee6999efdabdbd31
darzokako-maker <darzokako@gmail.com>
2026-05-17 16:03:54 +0300

    Initialize settings.gradle.kts for project configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MT APK Manager"
+include(":app")
+

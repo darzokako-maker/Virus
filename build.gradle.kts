3138ac0a85459a9cdca8f98a7ec49178587fdac2
darzokako-maker <darzokako@gmail.com>
2026-05-17 16:16:37 +0300

    Add dependencies for Android libraries and testing
    Added dependencies for core libraries and testing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
-// Üst düzey build dosyası; eklentileri alt modüllere (app) dağıtır.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material) // Burası libs.versions.toml'daki düzeltmeyi otomatik çekecek
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
-
